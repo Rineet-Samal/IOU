fe04c623f597cdf456321242c791cb11d4a8ed45
Rineet-Samal <165139314+Rineet-Samal@users.noreply.github.com>
2026-09-17 17:44:02 +0530

    feat: description changes

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 USERS_FILE = os.path.join(DATA_DIR, "users.json")
 
-
+# app is set up in render
 def ensure_data_file():
     os.makedirs(DATA_DIR, exist_ok=True)
     if not os.path.exists(USERS_FILE):
